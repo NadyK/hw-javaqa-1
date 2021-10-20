cd6266e28d52c78e0fe6337efe9ae6a6cb457ace
Надежда <90605537+NadyK@users.noreply.github.com>
2021-10-20 11:06:10 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        int a = 2000000000;
-        int b = 500000000;
+        int a = 2_000_000_000;
+        int b = 500_000_000;
         int total = a + b;
         System.out.println(total);
     }
